c85ecb65a52afff8398de141a2cd43a0e03d49e4
Dang2306 <ltchau15031996@gmail.com>
2024-05-06 23:38:43 +0700

    update

---
+++ src/main/java/SumOfDigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+  public static void main(String[] args) {
+    int n, res;
+    Scanner scanner = new Scanner(System.in);
+    do {
+      System.out.println("input n : ");
+      n = scanner.nextInt();
+    } while (n <= 0);
+    res = sum_digit_of_num(n);
+    System.out.println("Sum of digits of " + n + " is " + res);
+    scanner.close();
+  }
+
+  public static int sum_digit_of_num(int number) {
+    int sum = 0;
+    while (number > 0) {
+      sum += number % 10;
+      number = number / 10;
+    }
+    return sum;
+  }
+}
